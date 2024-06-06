576b150d8964fe6668501b175dae8a4b783158fa
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-06-06 02:07:12 +0200

    Creatd movies comps

--- src/app/(private)/movies/page.jsx
+++ src/app/(private)/movies/page.jsx
@@ -1,11 +1,31 @@
-import { getMovies } from "@/helpers/movieFunctions"
+import { getMovies } from "@/helpers/movieFunctions";
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import MovieSection from "./components/MovieSection";
 
-const Movies =async () => {
-  const movies = await getMovies("now_playing")
-  console.log(movies)
+export const metadata = {
+  title: "Movies",
+  description: "this is movies page  ",
+};
+
+const Movies = async () => {
+  const movies = await getMovies("now_playing");
+  // console.log(movies);
   return (
-    <div>Movies</div>
-  )
-}
+    <div>
+      <HeroSection
+        id={movies[0].id}
+        title={movies[0]?.title}
+        overview={movies[0]?.overview}
+      />
+      <div className="px-4 md:px-12 mt-4">
+        <MovieSection title="NOW PLAYING" type="now_playing" />
+        <MovieSection title="POPULAR" type="popular" />
+        <MovieSection title="TOP RATED" type="top_rated" />
+        <MovieSection title="UPCOMING" type="upcoming" />
+      </div>
+    </div>
+  );
+};
 
-export default Movies
+export default Movies;
